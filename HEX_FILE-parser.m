0b9d1d3d6123ba3cd6d200b701369085206348d2
MGhassab <mghassab.uni@gmail.com>
2021-03-30 18:52:02 +0430

    HEX-PARSER created

---
+++ HEX_FILE-parser.m
@@ -0,0 +1,12 @@
+clc
+close all;
+clear all;
+%%
+fid = fopen('internal_flash_test.hex');
+A   = fread(fid, Inf, 'uint8');
+B   = reshape(A,45,227);
+C   = B(10:41,:);
+fclose(fid);
+fid = fopen('example.txt', 'w');
+% fprintf(fid, '%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c \r\n', C);
+fprintf(fid, '%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,%c,\n', C);
