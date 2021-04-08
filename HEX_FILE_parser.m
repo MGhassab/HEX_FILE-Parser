a66d4e2e1a59c2eea2afb26908de51a1c854ed6e
MGhassab <mghassab.uni@gmail.com>
2021-04-08 17:56:55 +0430

    add commands

--- HEX_FILE_parser.m
+++ HEX_FILE_parser.m
@@ -1,12 +1,13 @@
 clc
 close all;
 clear all;
-%%
-fid = fopen('internal_flash_test.hex');
-A   = fread(fid, Inf, 'uint8');
-B   = reshape(A,45,227);
-C   = B(10:41,:);
+%% read hex file
+fid = fopen('application_program.hex');
+Hex_code   = fread(fid, Inf, 'uint8');
+%% Separate data 
+Reshape_HEX   = reshape(Hex_code,45,214);
+Data   = Reshape_HEX(10:41,:);
 fclose(fid);
+%% Make Output
 fid = fopen('example.txt', 'w');
-% fprintf(fid, '%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c \r\n', C);
-fprintf(fid, '0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,\n', C);
+fprintf(fid, '0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,0x%c%c,\n', Data);
